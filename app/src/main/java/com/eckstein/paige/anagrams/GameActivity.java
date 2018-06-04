e910d891fe85a878e148cb2af8573434cfc0dab3
NotQuiteHeroes <paige.eckstein@gmail.com>
2018-06-04 13:40:21 -0500

    Add word validation functionality.

--- app/src/main/java/com/eckstein/paige/anagrams/GameActivity.java
+++ app/src/main/java/com/eckstein/paige/anagrams/GameActivity.java
@@ -9,6 +9,7 @@ import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -16,10 +17,12 @@ import java.util.List;
 
 public class GameActivity extends Activity implements View.OnClickListener {
 
-    GamePlay play = new GamePlay();
-    HashMap<Integer, String> tiles;
-    TextView currentWord;
-    static final int[] button_ids =
+    private GamePlay play;
+    private HashMap<Integer, String> tiles;
+    private TextView currentWord;
+    private ArrayList<Integer> selectedLetters;
+
+    private static final int[] button_ids =
             {
                     R.id.c0r0, R.id.c0r1, R.id.c0r2, R.id.c0r3, R.id.c0r4,
                     R.id.c1r0, R.id.c1r1, R.id.c1r2, R.id.c1r3, R.id.c1r4,
@@ -34,7 +37,9 @@ public class GameActivity extends Activity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        GamePlay play = new GamePlay();
+        play = new GamePlay(this);
+        selectedLetters = new ArrayList<>();
+
         currentWord = findViewById(R.id.playField);
 
         tiles = new HashMap<>();
@@ -47,12 +52,23 @@ public class GameActivity extends Activity implements View.OnClickListener {
             tiles.put(id, play.getTileLetter(randomTile));
         }
 
+        Button clear = findViewById(R.id.clearButton);
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currentWord.setText("");
+                selectedLetters.clear();
+            }
+        });
+
         //this temporarily just takes the user to the end game screen
         //will eventually be used to submit button for verification process
         Button submit = findViewById(R.id.submitButton);
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String word = currentWord.getText().toString();
+                checkWord(word);
                 Intent intent = new Intent(GameActivity.this, GameOver.class);
                 startActivity(intent);
                 finish();
@@ -60,10 +76,26 @@ public class GameActivity extends Activity implements View.OnClickListener {
         });
     }
 
+    public void checkWord(String word)
+    {
+        if(play.validWord(word))
+        {
+            int score = play.scoreWord(word);
+            play.addWord(word, score);
+            //temporary --- for testing purposes
+            System.out.println("SCORE! "+word+"\t"+score);
+        }
+        else
+        {
+            System.out.println("ERROR! "+word+" is not a valid word.");
+        }
+    }
+
     public void onClick(View v)
     {
         Animation ranim = AnimationUtils.loadAnimation(this, R.anim.rotate);
         v.startAnimation(ranim);
-        currentWord.append(tiles.get(v.getId()));
+        currentWord.append((tiles.get(v.getId())).toUpperCase());
+        selectedLetters.add(v.getId());
     }
 }
--- app/src/main/java/com/eckstein/paige/anagrams/GamePlay.java
+++ app/src/main/java/com/eckstein/paige/anagrams/GamePlay.java
@@ -1,14 +1,59 @@
 package com.eckstein.paige.anagrams;
 
+import android.content.Context;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
 
 public class GamePlay {
 
+    //holds application context to access resources
+    private Context context;
+    //holds all possible valid English words
+    private Set<String> dictionary;
+    //hold all possible letter tiles with Scrabble style distribution
     private int[] tiles;
+    //used to randomly select tile for board from tiles array
     private Random rng;
+    //holds individual letter scores
+    private HashMap<Character, Integer> score;
+    //holds words and their scores made by the user during game play
+    private HashMap<String, Integer> wordScores;
 
-    public GamePlay()
+    /**
+     * Constructor
+     *
+     * @param context context passed by application, used to gain access
+     *                to resources
+     *
+     * Populates tiles array with Scrabble inspired distribution of letters:
+     *      a:.09   b:.02   c:.02   d:.04   e:.12   f:.02   g:.03
+     *      h:.02   i:.09   j:.02   k:.02   l:.04   m:.04   n:.04
+     *      o:.08   p:.02   q:.01   r:.06   s:.04   t:.06   u:.04
+     *      v:.02   w:.02   x:.01   y:.02   z:.01
+     *
+     * Initializes score HashMap <String, Integer> that contains individual letter
+     * scores in the form <"a":1> and populates it with hardcoded scores.
+     * The hardcoded scores can be modified in the setupScores module.
+     *
+     * Initializes empty HasHMap <String, Integer> that will be used to hold
+     * words submitted by the user during game play. Format will be <"Word":55>
+     * This will be used by the the GameOver activity to display words found
+     * during the game, as well as their accompanying score.
+     *
+     * Initialize Random Number Generator
+     */
+    public GamePlay(Context context)
     {
+        this.context = context;
+
         tiles = new int[]{R.drawable.a, R.drawable.a, R.drawable.a, R.drawable.a, R.drawable.a,
                           R.drawable.a, R.drawable.a, R.drawable.a, R.drawable.a, R.drawable.b,
                           R.drawable.b, R.drawable.c, R.drawable.c, R.drawable.d, R.drawable.d,
@@ -20,7 +65,7 @@ public class GamePlay {
                           R.drawable.i, R.drawable.i, R.drawable.i, R.drawable.i, R.drawable.i,
                           R.drawable.j, R.drawable.j, R.drawable.k, R.drawable.k, R.drawable.l,
                           R.drawable.l, R.drawable.l, R.drawable.l, R.drawable.m, R.drawable.m,
-                          R.drawable.n, R.drawable.n, R.drawable.n, R.drawable.n, R.drawable.n,
+                          R.drawable.m, R.drawable.m, R.drawable.n, R.drawable.n, R.drawable.n,
                           R.drawable.n, R.drawable.o, R.drawable.o, R.drawable.o, R.drawable.o,
                           R.drawable.o, R.drawable.o, R.drawable.o, R.drawable.o, R.drawable.p,
                           R.drawable.p, R.drawable.q_pink_text, R.drawable.r, R.drawable.r, R.drawable.r,
@@ -30,14 +75,195 @@ public class GamePlay {
                           R.drawable.u, R.drawable.u, R.drawable.v, R.drawable.v, R.drawable.w,
                           R.drawable.w, R.drawable.x_pink_text, R.drawable.y, R.drawable.y, R.drawable.z_pink_text};
 
+        score = new HashMap<>();
+        wordScores = new HashMap<>();
+
+        setupScore();
+        populateDictionary();
         rng = new Random();
     }
 
+    /**
+     * Populates dictionary with all valid alpha English words, pulled from list provided by:
+     *      dwyl: https://github.com/dwyl/english-words
+     * This locates the file in app, reads in words one by one,
+     * then creates Set of words
+     */
+    private void populateDictionary()
+    {
+        dictionary = new HashSet<>();
+        BufferedReader br = null;
+        try
+        {
+            InputStream is = context.getResources().openRawResource(R.raw.words);
+            br = new BufferedReader(new InputStreamReader(is));
+            String line = null;
+            while((line = br.readLine())!= null)
+            {
+                dictionary.add(line);
+            }
+        } catch (IOException io)
+        {
+            Log.e("DICTIONARY", "ERROR: "+io);
+        } finally
+        {
+            try {
+                if (br != null) {
+                    br.close();
+                }
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    /**
+     * Validates if a given input is a meaningful word in English
+     * returns true if valid, false otherwise
+     *
+     * Checks validity against dictionary of all accepted valid English words
+     * found in "words.txt" at root directory. List of words provided by:
+     *      dwyl: https://github.com/dwyl/english-words
+     *
+     * @param input user submitted String to test if valid word
+     * @return  true if user submitted String is a valid English word
+     *          false otherwise
+     */
+    public boolean validWord(String input)
+    {
+        return dictionary.contains(input.toLowerCase());
+    }
+
+    /**
+     * Called once a user submitted word has been validated
+     * Used to tally a score for a given word
+     * Determining letter scores are held in the score HasHMap <Character, Integer>
+     * @param word  String: validated user submitted word to be scored
+     * @return  integer score for submitted word
+     */
+    public int scoreWord(String word)
+    {
+        int score = 0;
+        word = word.toLowerCase();
+
+        for(int i = 0; i < word.length(); i++)
+        {
+            score += getLetterScore(word.charAt(i));
+        }
+
+        return score;
+    }
+
+    /**
+     * getLetterScore returns the integer score of an individual
+     * letter
+     *
+     * Example:
+     * Input: "a"
+     * Output: 1
+     *
+     * Queries the score HashMap for letter-score mapping
+     *
+     * @param letter the individual letter to retrieve the score of
+     * @return  integer score of individual letter
+     */
+    public int getLetterScore(char letter)
+    {
+        return score.get(letter);
+    }
+
+    /**
+     * addWord is called when a user has submitted a valid word
+     * during game play.
+     * The word is scored and added to the wordScores HashMap
+     * <String, Integer> in the form:
+     *      <"word", 10>
+     *
+     *  This HashMap will be used to display final words and
+     *  their resulting scores in the GameOver activity
+     *
+     * @param word String: validated and scored word submitted
+     *             by the user during game play
+     * @param score integer: corresponding score of the validated
+     *              word submitted by the user (@param word)
+     */
+    public void addWord(String word, int score)
+    {
+        wordScores.put(word, score);
+    }
+
+    /**
+     * getWordScores returns the entire wordScores HashMap
+     * <String, Integer>.
+     * This HashMap contains all the validated words the
+     * user has submitted during a single game play instance
+     * along with the word's corresponding score.
+     *
+     * @return wordScores HashMap<String, Integer> containing
+     *          all validated words submitted by the user
+     *          during game play along with their corresponding
+     *          scores. HashMap of form:
+     *          <"word", 10>
+     */
+    public HashMap<String, Integer> getWordScores() {
+        return wordScores;
+    }
+
+    /**
+     * getRandomTile randomly selects a drawable's id from the tiles
+     * array. This uses a scrabble style distribution of letters
+     * and can be modified in the default constructor of this class.
+     *
+     * @return  integer drawable id mapped to a letter tile image
+     *           randomly selected from tiles array
+     */
     public int getRandomTile()
     {
         return tiles[rng.nextInt(99)];
     }
 
+    /**
+     * setupScore populates the score HashMap <Character, Integer>
+     * This HashMap holds the score of an individual letter in
+     * the form <'a':1>
+     */
+    private void setupScore()
+    {
+        score.put('a', 1);
+        score.put('b', 3);
+        score.put('c', 3);
+        score.put('d', 3);
+        score.put('e', 1);
+        score.put('f', 3);
+        score.put('g', 3);
+        score.put('h', 3);
+        score.put('i', 1);
+        score.put('j', 5);
+        score.put('k', 5);
+        score.put('l', 1);
+        score.put('m', 1);
+        score.put('n', 1);
+        score.put('o', 1);
+        score.put('p', 3);
+        score.put('q', 10);
+        score.put('r', 1);
+        score.put('s', 1);
+        score.put('t', 1);
+        score.put('u', 1);
+        score.put('v', 5);
+        score.put('w', 5);
+        score.put('x', 10);
+        score.put('y', 5);
+        score.put('z', 10);
+    }
+
+    /**
+     * getTileLetter returns the String letter of a tile's image
+     * @param drawable integer drawable id of letter tile image
+     *                 to retrieve String letter from
+     * @return String version of what is displayed on tile's image
+     */
     public String getTileLetter(int drawable)
     {
        switch(drawable)
